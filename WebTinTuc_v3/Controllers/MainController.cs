933109236e939498c78954e004894663792ba312
nrostrungkien3779@gmail.com <nrostrungkien3779@gmail.com>
2021-04-11 23:31:15 +0700

    first commit

---
+++ WebTinTuc_v3/Controllers/MainController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebTinTuc_v3.Models;
+
+namespace WebTinTuc_v3.Controllers
+{
+    public class MainController : Controller
+    {
+        WebTinTucEntities db = new WebTinTucEntities();
+        // GET: Main
+        public ActionResult Index ()
+        {
+            return View();
+        }
+        public ActionResult Detail ()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Login(TAIKHOAN t)
+        {
+            var user = db.TAIKHOANs.Where(s => s.ID == t.ID && s.Password == t.Password).FirstOrDefault();
+            //var checkRole = db.TAIKHOANs.Where(s => s.LoaiTaiKhoan == t.LoaiTaiKhoan).FirstOrDefault();
+            if (user == null)
+            {
+                return PartialView();
+            }
+            else
+            {
+                db.Configuration.ValidateOnSaveEnabled = false;
+                Session["ID"] = t.ID;
+                Session["Password"] = t.Password;
+
+                //Error Role
+                if (user.LoaiTaiKhoan.Contains("Admin"))
+                {
+                    return RedirectToAction("Main", "Admin");
+                }
+                else if(user.LoaiTaiKhoan.Contains("NguoiDuyetBai"))
+                {
+                    
+                }
+                else if (user.LoaiTaiKhoan.Contains("NguoiDangBai"))
+                {
+
+                }   
+                else
+                {
+                    
+                }
+                return PartialView();
+            }
+        }
+    }
+}
